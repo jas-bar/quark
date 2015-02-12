d898a124f45bc97402b7ddbf23b54fc535c725ef
tomsik68 <tomsik68@gmail.com>
2015-02-12 16:22:04 +0100

    Created unified header file for the entire library
    Inspired by SDL.h :)

---
+++ quark.h
@@ -0,0 +1,13 @@
+#ifndef QUARK_H
+#define QUARK_H
+
+#include "input.h"
+#include "quarkgame.h"
+#include "quarkgamecontainer.h"
+#include "quarkgameloader.h"
+#include "quarkgamestate.h"
+#include "quarkresource.h"
+#include "quarkresourceloader.h"
+#include "quarkresources.h"
+
+#endif // QUARK_H
