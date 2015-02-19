69a90e0dd08ea90c1cb3829d1f1e5a5f2d4a8a31
tomsik68 <tomsik68@gmail.com>
2015-02-19 19:16:09 +0100

    Added InputListener interface

---
+++ inputlistener.h
@@ -0,0 +1,12 @@
+#ifndef INPUTLISTENER_H
+#define INPUTLISTENER_H
+
+#include <SDL_events.h>
+
+class InputListener
+{
+public:
+    virtual void onEvent(SDL_Event* event) = 0;
+};
+
+#endif // INPUTLISTENER_H
